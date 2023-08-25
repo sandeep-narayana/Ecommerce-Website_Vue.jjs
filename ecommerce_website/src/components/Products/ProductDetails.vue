5df2ff3cdbcec7f10bf07db2b25f59b995b2e731
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-08-25 12:48:18 +0530

    implimnet add to cart and add to wishlist button on product display

--- ecommerce_website/src/components/Products/ProductDetails.vue
+++ ecommerce_website/src/components/Products/ProductDetails.vue
@@ -4,7 +4,11 @@
       <div class="col-md-1"></div>
       <!-- display image -->
       <div class="col-md-4 col-12">
-        <img :src="displayProduct.image" alt="" class="img-fluid" />
+        <img
+          :src="displayProduct.image"
+          alt=""
+          class="img-fluid custom-image"
+        />
       </div>
       <!-- Display product Details -->
       <div class="col-md-6 col-12 pt-3 pt-md-0">
@@ -22,11 +26,23 @@
             <li>Lorem ipsum dolor sit amet consectetur adipisicing</li>
           </ul>
         </div>
+        <!-- // buttons for Add to cart and Add to WishList -->
+        <div class="card-footer d-flex mb-2 justify-content-around">
+          <button class="btn btn-success mb-2" @click="AddToCart()">
+            Add to Cart
+          </button>
+          <button class="btn btn-primary mb-2" @click="AddToWishList()">
+            Add to WishList
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
 <script>
+import axios from "axios";
+import sweetAlret from "sweetalert2";
 export default {
   name: "ProductDetails",
   data() {
@@ -35,7 +51,7 @@ export default {
       category: {},
     };
   },
-  props: ["products", "categoreis"],
+  props: ["products", "categoreis", "user"],
   mounted() {
     this.id = this.$route.params.id;
     this.displayProduct = this.products.find(
@@ -53,11 +69,88 @@ export default {
       }
     }
   },
+  methods: {
+    // Add product to Cart and delete from Wishlist
+    async AddToCart() {
+      try {
+        if (this.user == null) {
+          sweetAlret.fire({
+            icon: "error",
+            title: "login first",
+          });
+          return;
+        }
+
+        const userId = this.user.id;
+        // Make a GET request to get the user's data including the cart and wishlist
+        const response = await axios.get(
+          `http://localhost:3000/users/${userId}`
+        );
+
+        // Extract the user data
+        const userDataFromDb = response.data;
+
+        // Add the product to the wishlist
+        const updatedCart = [...userDataFromDb.cart, this.displayProduct];
+        userDataFromDb.cart = updatedCart;
+
+        // Update both cart and wishlist in a single PUT request
+        await axios.put(
+          `http://localhost:3000/users/${userId}`,
+          userDataFromDb
+        );
+
+        // Emit an event to notify the parent component to update its state
+        this.$emit("fetchData");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    // Add product to WishList
+    async AddToWishList() {
+      try {
+        if (this.user == null) {
+          sweetAlret.fire({
+            icon: "error",
+            title: "login first",
+          });
+          return;
+        }
+
+        const userId = this.user.id;
+        // Make a GET request to get the user's data including the cart and wishlist
+        const response = await axios.get(
+          `http://localhost:3000/users/${userId}`
+        );
+
+        // Extract the user data
+        const userDataFromDb = response.data;
+
+        // Add the product to the wishlist
+        const updatedWishList = [
+          ...userDataFromDb.wishlist,
+          this.displayProduct,
+        ];
+        userDataFromDb.wishlist = updatedWishList;
+
+        // Update user
+        await axios.put(
+          `http://localhost:3000/users/${userId}`,
+          userDataFromDb
+        );
+
+        // Emit an event to notify the parent component to update its state
+        this.$emit("fetchData");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
 <style>
-.category{
-    font-weight: 400px;
-    
+.category {
+  font-weight: 400px;
 }
 </style>
